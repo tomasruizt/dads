64cdc7cf148ebf79169619deabb332955afa2a84
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2020-10-12 17:35:55 +0200

    Remove fetch timelimit, add fixed goal

--- envs/fetch.py
+++ envs/fetch.py
@@ -1,10 +1,17 @@
 from functools import partial
-
+import numpy as np
 from gym.envs.robotics import FetchPickAndPlaceEnv
-from gym.wrappers import TimeLimit, FilterObservation, FlattenObservation
+from gym.wrappers import FilterObservation, FlattenObservation
+
+
+class FixedGoalFetchPickAndPlaceEnv(FetchPickAndPlaceEnv):
+    _fixed_goal = np.asarray([1.34803644, 0.71081931, 0.6831472])
+
+    def _sample_goal(self):
+        return self._fixed_goal.copy()
 
 
 def make_fetch_pick_and_place_env():
     filter_obs = partial(FilterObservation, filter_keys=["observation", "achieved_goal"])
-    env = FetchPickAndPlaceEnv()
-    return TimeLimit(max_episode_steps=100, env=FlattenObservation(filter_obs(env)))
+    env = FixedGoalFetchPickAndPlaceEnv(reward_type="dense")
+    return FlattenObservation(filter_obs(env))
