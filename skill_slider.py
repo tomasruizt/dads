fb2965cc4855e16503f063e43fb155bb0a012d21
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2020-11-04 10:39:28 +0100

    slider normalized to [-1,1]

--- skill_slider.py
+++ skill_slider.py
@@ -9,6 +9,8 @@ class SlidersInputFrame(wx.Frame):
         super().__init__(parent=None, title="Sliders", size=(400, dim*100 - 20))
         panel = wx.Panel(self)
         sizer = wx.BoxSizer(wx.VERTICAL)
+        title = wx.StaticText(panel, style=wx.ALIGN_RIGHT, label="Percentages of 1 (5 means 0.05).")
+        sizer.Add(title, 0, wx.ALIGN_CENTER)
         self._sliders = [_make_slider(panel) for _ in range(dim)]
         for slider in self._sliders:
             sizer.Add(slider, 0, wx.ALL | wx.EXPAND, 5)
@@ -16,12 +18,12 @@ class SlidersInputFrame(wx.Frame):
         self.Show()
 
     def get_slider_values(self):
-        return [s.GetValue() for s in self._sliders]
+        return [s.GetValue() / 100.0 for s in self._sliders]
 
 
 def _make_slider(parent):
     style = wx.SL_MIN_MAX_LABELS | wx.SL_VALUE_LABEL
-    return wx.Slider(parent, value=0.0, minValue=-3.5, maxValue=3.5, style=style)
+    return wx.Slider(parent, value=0.0, minValue=-100, maxValue=100, style=style)
 
 
 def create_sliders_widget(dim=5) -> SlidersInputFrame:
@@ -37,4 +39,4 @@ if __name__ == '__main__':
     widget = create_sliders_widget()
     while True:
         print(widget.get_slider_values(), flush=True)
-        time.sleep(0.05)
+        time.sleep(0.1)
