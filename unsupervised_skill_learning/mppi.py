9d447be22d5ecf701106cfd650db060d18badc8a
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2020-11-06 10:34:52 +0100

    Refactor mppi

--- unsupervised_skill_learning/mppi.py
+++ unsupervised_skill_learning/mppi.py
@@ -48,6 +48,7 @@ def eval_mppi(
      (avoid using).
      top_primitives: number of elites to choose, if using CEM (not tested).
     """
+    check_reward_fn(env)
     steps_per_skill = primitive_horizon
     num_skills_to_plan = planning_horizon
 
@@ -55,7 +56,6 @@ def eval_mppi(
             dynamics=dynamics,
             prior_type=prior_type,
             skill_dim=latent_action_space_size,
-            episode_len=episode_horizon,
             num_skills_to_plan=num_skills_to_plan,
             steps_per_skill=steps_per_skill,
             refine_steps=refine_steps,
@@ -86,11 +86,20 @@ def eval_mppi(
             print(f"MPC step {step}")
 
 
+def check_reward_fn(env):
+    obs1 = env.observation_space.sample()
+    obs2 = env.observation_space.sample()
+    reward_dads = env.compute_reward(obs1, obs2, info="dads")
+
+    to_goal = env.unwrapped.achieved_goal_from_state
+    reward_classic = env.compute_reward(to_goal(obs1), to_goal(obs2), info=None)
+    assert not np.isclose(reward_dads, reward_classic), f"Both rewards are equal to: {reward_dads}"
+
+
 def choose_next_skill_loop(dynamics,
                            num_skills_to_plan: int,
                            prior_type: str,
                            skill_dim: int,
-                           episode_len: int,
                            steps_per_skill: int,
                            refine_steps: int,
                            num_candidate_sequences,
@@ -107,39 +116,37 @@ def choose_next_skill_loop(dynamics,
 
     while True:
         cur_timestep = yield
-        for _ in range(episode_len // steps_per_skill):
-            for _ in range(refine_steps):
-                # generate candidates sequences for primitives
-                candidate_skills_seqs = []
-                for _ in range(num_candidate_sequences):
-                    skill_seq = [sample_skills(skills_distr_params[idx], prior_type=prior_type) for idx in range(num_skills_to_plan)]
-                    candidate_skills_seqs.append(skill_seq)
-                candidate_skills_seqs = smooth(np.asarray(candidate_skills_seqs), beta=smoothing_beta)
+        for _ in range(refine_steps):
+            candidate_skills_seqs = []
+            for _ in range(num_candidate_sequences):
+                skill_seq = [sample_skills(skills_distr_params[idx], prior_type=prior_type) for idx in range(num_skills_to_plan)]
+                candidate_skills_seqs.append(skill_seq)
+            candidate_skills_seqs = smooth(np.asarray(candidate_skills_seqs), beta=smoothing_beta)
 
-                def process(obs):
-                    return process_observation_given(obs, env_name=env_name, reduced_observation=reduced_observation)
+            def process(obs):
+                return process_observation_given(obs, env_name=env_name, reduced_observation=reduced_observation)
 
-                running_cur_state = np.array([process(cur_timestep.observation)] * num_candidate_sequences)
-                running_reward = np.zeros(num_candidate_sequences)
-                for planning_idx in range(num_skills_to_plan):
-                    cur_skills = candidate_skills_seqs[:, planning_idx, :]
-                    for _ in range(steps_per_skill):
-                        predicted_next_state = dynamics.predict_state(running_cur_state, cur_skills)
+            running_cur_state = np.array([process(cur_timestep.observation)] * num_candidate_sequences)
+            running_reward = np.zeros(num_candidate_sequences)
+            for planning_idx in range(num_skills_to_plan):
+                cur_skills = candidate_skills_seqs[:, planning_idx, :]
+                for _ in range(steps_per_skill):
+                    predicted_next_state = dynamics.predict_state(running_cur_state, cur_skills)
 
-                        # update running stuff
-                        dense_reward = env_compute_reward_fn(running_cur_state, predicted_next_state, info="dads")
-                        # modification for sparse_reward
-                        if sparsify_rewards:
-                            sparse_reward = 5.0 * (dense_reward > -2) + 0.0 * (
-                                    dense_reward <= -2)
-                            running_reward += sparse_reward
-                        else:
-                            running_reward += dense_reward
+                    # update running stuff
+                    dense_reward = env_compute_reward_fn(running_cur_state, predicted_next_state, info="dads")
+                    # modification for sparse_reward
+                    if sparsify_rewards:
+                        sparse_reward = 5.0 * (dense_reward > -2) + 0.0 * (
+                                dense_reward <= -2)
+                        running_reward += sparse_reward
+                    else:
+                        running_reward += dense_reward
 
-                        running_cur_state = predicted_next_state
+                    running_cur_state = predicted_next_state
 
-                update_parameters(candidate_skills_seqs, running_reward, skills_distr_params,
-                                  prior_type=prior_type, mppi_gamma=mppi_gamma, top_primitives=top_primitives)
+            update_parameters(candidate_skills_seqs, running_reward, skills_distr_params,
+                              prior_type=prior_type, mppi_gamma=mppi_gamma, top_primitives=top_primitives)
 
         chosen_skill = get_mean_skill(skills_distr_params[0], prior_type=prior_type)
         yield chosen_skill
@@ -184,10 +191,11 @@ def update_parameters(candidates, reward, primitive_parameters,
     if prior_type == 'normal':
         reward = np.exp(mppi_gamma * (reward - np.max(reward)))
         reward = reward / (reward.sum() + 1e-10)
-        new_means = (candidates.T * reward).T.sum(axis=0)
+        new_means = np.average(candidates, axis=0, weights=reward)
 
-        for planning_idx in range(candidates.shape[1]):
-            primitive_parameters[planning_idx][0] = new_means[planning_idx]
+        for idx, mean in enumerate(new_means):
+            primitive_parameters[idx][0] = mean
+        return
 
     # TODO(architsh): closer to cross-entropy/shooting method, figure out a better update
     elif prior_type == 'uniform':
