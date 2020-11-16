dece288a34a512dc5cee6e6bd869da6b86b6b0c0
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2020-11-16 19:07:22 +0100

    move mppi to GPU

--- unsupervised_skill_learning/custom_mppi.py
+++ unsupervised_skill_learning/custom_mppi.py
@@ -14,24 +14,26 @@ class MPPISkillProvider(SkillProvider):
         self._dynamics = dynamics
         dyn_obs_dim = len(env.to_dynamics_obs(env.observation_space.sample()))
         action_dim = dynamics._action_size
+        self._device = "cuda"
         self._planner = MPPI(dynamics=self._dynamics_fn,
                              running_cost=self._cost_fn,
                              nx=dyn_obs_dim,
                              u_min=-torch.ones(action_dim), u_max=torch.ones(action_dim),
-                             noise_sigma=0.1*torch.eye(action_dim), device="cpu", horizon=skills_to_plan, lambda_=1e-6)
+                             noise_sigma=0.1*torch.eye(action_dim), device=self._device, horizon=skills_to_plan, lambda_=1e-6)
 
     def _dynamics_fn(self, state: torch.Tensor, actions: torch.Tensor) -> torch.Tensor:
-        res = self._dynamics.predict_state(timesteps=state.numpy(), actions=actions.numpy())
-        return torch.tensor(res)
+        res = self._dynamics.predict_state(timesteps=state.cpu().numpy(), actions=actions.cpu().numpy())
+        return torch.tensor(res, device=self._device)
 
     def _cost_fn(self, dyn_obs: torch.Tensor, actions: torch.Tensor) -> torch.Tensor:
-        next_dyn_obs = self._dynamics.predict_state(timesteps=dyn_obs.numpy(), actions=actions.numpy())
-        reward = self._env.compute_reward(dyn_obs, next_dyn_obs, info=DADSEnv.OBS_TYPE.DYNAMICS_OBS)
-        return torch.tensor(-reward)
+        cur_dyn_obs = dyn_obs.cpu().numpy()
+        next_dyn_obs = self._dynamics.predict_state(timesteps=cur_dyn_obs, actions=actions.cpu().numpy())
+        reward = self._env.compute_reward(cur_dyn_obs, next_dyn_obs, info=DADSEnv.OBS_TYPE.DYNAMICS_OBS)
+        return torch.tensor(-reward, device=self._device)
 
     def start_episode(self):
         self._planner.reset()
 
     def get_skill(self, ts: TimeStep) -> np.ndarray:
         dyn_obs = self._env.to_dynamics_obs(ts.observation)
-        return self._planner.command(state=dyn_obs).numpy()
+        return self._planner.command(state=dyn_obs).cpu().numpy()
