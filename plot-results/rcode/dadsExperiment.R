8a7e6ec8fbda3c704a7cc81d34ae1ffbbe389e47
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2020-12-20 14:00:08 +0100

    Update plotting code

--- plot-results/rcode/dadsExperiment.R
+++ plot-results/rcode/dadsExperiment.R
@@ -3,8 +3,8 @@ library(ggplot2)
 # svg aspect: 500px times 350px
 
 setwd("/tomasruiz/code/thesis/dads/")
-results <- fread("modelsCommandSkills/reach/asGDADSTrue/full-results.csv")
-results[, ITERATION := ITERATION / 500L]
+results <- fread("plot-results/raw-data/dads-push/push-w-resampling.csv")
+#results[, ITERATION := ITERATION / 500L]
 
 name <- function(goalSpace, resampling) {
     if (!goalSpace && !resampling)
@@ -14,7 +14,6 @@ name <- function(goalSpace, resampling) {
     if (goalSpace)
         return("Goal-Space Control")
 }
-trStr <- "TRANSITIONS_MOVING_GOAL[%]"
 
 preprocess <- function(DT) {
     DT[, ABLATION := mapply(name, CONTROL_GOAL_SPACE, USE_RESAMPLING)]
@@ -23,10 +22,10 @@ preprocess <- function(DT) {
 
 means <- preprocess(results)
 
-
 dadsReachResults <- function() {
     color <- "blue"
     ablation <- "Goal-Space Control"
+    ablation <- "Resampling Scheme"
     taskcompletion <- means[ABLATION == ablation & MEASUREMENT == "IS_SUCCESS"]
     taskcompletion <- taskcompletion[, .(ITERATION, COMPLETION=100*VALUE, SD=100*SD)]
     plotAvgTaskCompletion(DT = taskcompletion)
@@ -68,8 +67,10 @@ plotAvgTaskCompletion <- function(DT, color = "blue") {
 
 dadsPushDiagnostic <- function() {
     color <- "brown"
-    diagnostic <- means[ABLATION == "DADS" & MEASUREMENT == trStr & ITERATION < 1500]
-    diagnostic <- diagnostic[, .(ITERATION, PERCENTAGE=100*VALUE, SD)]
+    measurement <- "BUFFER-TRANSITIONS_MOVING_GOAL[%]"
+    measurement <- "DYNTRAIN-TRANSITIONS_MOVING_GOAL[%]"
+    diagnostic <- means[ABLATION == "Resampling Scheme" & MEASUREMENT == measurement & ITERATION < 1500]
+    diagnostic <- diagnostic[, .(ITERATION, PERCENTAGE=100*VALUE, SD=SD)]
     ggplot(diagnostic, aes(x = ITERATION, y = PERCENTAGE)) +
         geom_line(size = 1, color = color) +
         geom_ribbon(aes(ymax = PERCENTAGE + SD, ymin = PERCENTAGE - SD), fill = color, alpha = 0.3, color = NA) +
@@ -78,22 +79,37 @@ dadsPushDiagnostic <- function() {
 }
 
 
-plotData <- means[ABLATION == "DADS" & MEASUREMENT %in% c("PSEUDOREWARD", trStr)]
-ggplot(plotData, aes(x = ITERATION, y = VALUE, color = ABLATION)) +
-    facet_grid(MEASUREMENT ~ ABLATION, scales = "free") +
-    geom_line(size = 1) +
-    geom_ribbon(aes(ymax = VALUE + SD, ymin = VALUE - SD, fill = ABLATION),
-                alpha = 0.3, color = NA)
+plotPredicitonError <- function() {
+    ablation <- "Resampling Scheme"
+    prs <- c("PSEUDOREWARD_MOVING", "PSEUDOREWARD_NONMOVING")
+    errorData <- results[MEASUREMENT %like% "DYN_L2_ERROR" | MEASUREMENT %in% prs & ABLATION == ablation]
+    errorData[MEASUREMENT %like% "NONMOVING", `Transition Type` := "static"]
+    errorData[!(MEASUREMENT %like% "NONMOVING"), `Transition Type` := "moving"]
+    errorData[MEASUREMENT %in% prs, ]
+    errorData[MEASUREMENT == "DYN_L2_ERROR_NONMOVING_GOAL", MEASUREMENT := "Non-moving transitions"]
+    ggplot(errorData, aes(x = ITERATION, y = VALUE, color = MEASUREMENT)) +
+        geom_point(size = 1) + ggtitle("DADS Dynamics Mean Squared Error")
 
-transData <- means[MEASUREMENT == "PSEUDOREWARD"]
-ggplot(transData, aes(x = ITERATION, y = VALUE, color = ABLATION)) +
-    geom_line(size = 1) +
-    geom_ribbon(aes(ymax = VALUE + SD, ymin = VALUE - SD, fill = ABLATION),
-                alpha = 0.3, color = NA) + ggtitle("Transitions that move the goal [%]") +
-    ylim(-1, 100)
+}
 
-errorData <- results[MEASUREMENT %like% "DYN_L2_ERROR" & ABLATION == "DADS"]
-errorData[MEASUREMENT == "DYN_L2_ERROR_MOVING_GOAL", MEASUREMENT := "Moving transitions"]
-errorData[MEASUREMENT == "DYN_L2_ERROR_NONMOVING_GOAL", MEASUREMENT := "Non-moving transitions"]
-ggplot(errorData, aes(x = ITERATION, y = VALUE, color = MEASUREMENT)) +
-    geom_point(size = 1) + ggtitle("DADS Dynamics Mean Squared Error")
+pushComparePredictionErrorToIntrinsicReward <- function(){
+    ablation <- "Resampling Scheme"
+    errorData <- results[MEASUREMENT %like% "DYN_L2_ERROR" |
+                             MEASUREMENT %like% "PSEUDOREWARD_" & ABLATION == ablation]
+    errorData[MEASUREMENT %like% "NONMOVING", `Transition Type` := "static"]
+    errorData[!(MEASUREMENT %like% "NONMOVING"), `Transition Type` := "moving"]
+    errorData[MEASUREMENT %like% "DYN_L2_ERROR", METRIC := "Dynamics Prediction Error"]
+    errorData[!(MEASUREMENT %like% "DYN_L2_ERROR"), METRIC := "Intrinsic Reward"]
+    errorData$METRIC <- factor(errorData$METRIC, levels = c("Dynamics Prediction Error", "Intrinsic Reward"))
+    errorData <- errorData[, .(ITERATION, SEED, VALUE, `Transition Type`, METRIC)]
+    ggplot(errorData, aes(x = ITERATION, y = VALUE, color = `Transition Type`)) +
+        facet_wrap(vars(METRIC), scales = "free") +
+        ylab("") +
+        geom_point(size = 1, alpha = 0.6) +
+        ggtitle("High Intrinsic Reward Despite Unpredictability")
+
+    wide <- pivot_wider(errorData[ITERATION > 100], names_from = c(METRIC), values_from = VALUE)
+    ggplot(wide, aes(x = `Intrinsic Reward`, y = log(`Dynamics Prediction Error`), color = `Transition Type`)) +
+        geom_point(size = 1)
+}
+
